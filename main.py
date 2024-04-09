51a3a4d85185ff92801b05c471b5cfb47f3e5cbc
bobbyhadz <opmftw@gmail.com>
2024-04-09 09:08:34 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Check if a number is divisible by another number in Python
+
+if 9 % 3 == 0:
+    # 👇️ this runs
+    print('number A is divisible by number B')
+else:
+    print('numebr A is NOT divisible by number B')
+
+if 15 % 5 == 0:
+    # 👇️ this runs
+    print('number A is divisible by number B')
+else:
+    print('number A is NOT divisible by number B')
